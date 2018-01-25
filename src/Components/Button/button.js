096809c837af8ca9191a00d76860d75bb85d74df
Mayank Srivastav <mayanksri18@yahoo.in>
2018-01-25 22:13:12 +0530

    wrote Button component method

--- src/Components/Button/button.js
+++ src/Components/Button/button.js
@@ -1,1 +1,10 @@
 import React from "react";  
+
+const Button = props => (
+  <button 
+    id="btn-create-event"
+    onClick={props.onClick} 
+    className="btn btn-success">
+    {props.name}    
+  </button>
+);
