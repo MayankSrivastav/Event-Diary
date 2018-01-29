8048fda087f0a5fb03d25d5591a3645e88bbc703
Mayank Srivastav <mayanksri18@yahoo.in>
2018-01-29 21:46:09 +0530

    updated code for Calendar component

--- src/Components/Calendar/calendar.js
+++ src/Components/Calendar/calendar.js
@@ -2,3 +2,12 @@
 import React, { Component } from 'react';
 import DateTime from 'react-datetime';
 import './react-datetime.css';
+
+const Calender = props => (
+  <DateTime 
+    open={props.isOpen}
+    timeFormat={false} 
+    onChange={e => props.handleDateChange(e)}
+    value={props.value}
+  />
+);
