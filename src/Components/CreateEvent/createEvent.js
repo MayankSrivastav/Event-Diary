3c7175ff0ea0643821f8a4105e2451ab874df354
Mayank Srivastav <mayanksri18@yahoo.in>
2018-02-18 20:46:35 +0530

    updated code

--- src/Components/CreateEvent/createEvent.js
+++ src/Components/CreateEvent/createEvent.js
@@ -1,1 +1,7 @@
-
+import React, { Component } from 'react';
+import Calender from '../Calender/calender';
+import Button from '../Button/button';
+import TextBox from '../TextBox/textbox';
+import { store } from '../../index';
+import { createEvent } from '../../actions/actionCreater';
+import { connect } from 'react-redux';
