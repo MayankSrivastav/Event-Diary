63feb5e70ca9c584574110c6307aac743871b61a
Mayank Srivastav <mayanksri18@yahoo.in>
2018-02-19 22:02:22 +0530

    created CreateEvent component

--- src/Components/CreateEvent/createEvent.js
+++ src/Components/CreateEvent/createEvent.js
@@ -5,3 +5,17 @@ import TextBox from '../TextBox/textbox';
 import { store } from '../../index';
 import { createEvent } from '../../actions/actionCreater';
 import { connect } from 'react-redux';
+
+class CreateEvent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      date: ''
+    };
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+}
