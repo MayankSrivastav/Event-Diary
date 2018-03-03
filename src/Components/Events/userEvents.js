125caac37450334e3db48ae2122beeb367c5fa6d
Mayank Srivastav <mayanksri18@yahoo.in>
2018-03-03 21:01:14 +0530

    added import statements for the component

--- src/Components/Events/userEvents.js
+++ src/Components/Events/userEvents.js
@@ -1,1 +1,7 @@
 
+// user events presentation component
+
+import React, { Component } from 'react';
+import { store } from '../../index';
+import { fetchAllEvents } from '../../actions/actionCreater';
+import { connect } from 'react-redux';
