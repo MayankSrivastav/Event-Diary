84ce41b05c7f75258463a9583dabe7771e67c5c3
Mayank Srivastav <mayanksri18@yahoo.in>
2018-03-04 21:00:56 +0530

    created component userEvents

--- src/Components/Events/userEvents.js
+++ src/Components/Events/userEvents.js
@@ -5,3 +5,11 @@ import React, { Component } from 'react';
 import { store } from '../../index';
 import { fetchAllEvents } from '../../actions/actionCreater';
 import { connect } from 'react-redux';
+
+class UserEvents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    }
+}
