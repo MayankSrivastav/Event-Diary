eb9863408adf4fa1167e12e3f4eea8d163a49e86
Mayank Srivastav <mayanksri18@yahoo.in>
2018-03-09 21:56:27 +0530

    updated code for textbox component

--- src/Components/TexBox/textbox.js
+++ src/Components/TexBox/textbox.js
@@ -1,1 +1,15 @@
 
+import React from 'react';
+
+const TextBox = props => (
+    <input 
+      className="form-control" 
+      id={props.id} 
+      type="text" 
+      placeholder={props.placeholder}
+      onChange={e => props.handleTextChange(e)}
+      value={props.value}
+    />
+);
+
+export default TextBox;
