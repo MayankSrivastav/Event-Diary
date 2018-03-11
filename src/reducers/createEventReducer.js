86e3437b97d533e2bd05441d38c5df11f4b2eec5
Mayank Srivastav <mayanksri18@yahoo.in>
2018-03-11 21:00:40 +0530

    wrote the event reducer

--- src/reducers/createEventReducer.js
+++ src/reducers/createEventReducer.js
@@ -1,1 +1,13 @@
 
+import { initialState } from '../store/configureStore';
+
+const createEventReducer = (state = [], action) => {  
+  switch (action.type) {
+    case 'CREATE_EVENT':
+      return (
+        [action.event, ...state]
+      );
+    default:
+      return state;
+  }
+};
