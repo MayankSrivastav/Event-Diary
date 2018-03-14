a5152a6ed434969da73ccf33363e52e29fdafb3e
Mayank Srivastav <mayanksri18@yahoo.in>
2018-03-14 21:40:16 +0530

    wrote fetchEventsReducers method

--- src/reducers/fetchEventsReducers/fetchEventsReducers.js
+++ src/reducers/fetchEventsReducers/fetchEventsReducers.js
@@ -1,1 +1,11 @@
 
+const fetchEventsReducers = (state =[], action) => {
+  switch (action.type) {
+    case 'FETCH_ALL_EVENTS':
+      return (
+        state
+      );
+    default:
+      return state;
+  }
+};
