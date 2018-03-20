301078c4ba2f3a36c8d19760b98f9f558f24ee1c
Mayank Srivastav <mayanksri18@yahoo.in>
2018-03-20 21:27:07 +0530

    wrote combineReducer method

--- src/reducers/indexReducer.js
+++ src/reducers/indexReducer.js
@@ -2,3 +2,8 @@
 import { combineReducers } from 'redux';
 import createEventReducer from '../reducers/createEventReducer';
 // import fetchEventsReducer from '../reducers/fetchEventsReducer';
+
+const reducer = combineReducers({
+  allEvents: createEventReducer,
+  // allEvents: fetchEventsReducer
+});
